a7f87dc0af5d8b8d3d07f98875597c9752014de7
PedroMilani04 <pedrohenriquehmv@gmail.com>
2022-02-16 14:34:22 -0300

    commit6

---
+++ EX014REPET.js
@@ -0,0 +1,11 @@
+var cont = 1 
+while (cont <= 600) { // While
+    console.log(`Funcionando! Passo ${cont}`)
+    cont++ // Incrementa a variavel de 1 em 1
+}
+
+var con = 1
+do { // Do While (Inversa)
+    console.log(`Funcionando! Passo ${con}`)
+    con++
+} while (con <= 50)
